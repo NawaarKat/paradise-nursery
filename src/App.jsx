84ed9c27b16802380c04e42481a90a824168b0a9
Daniel Ovalle Tálaga <ovalle.talaga@gmail.com>
2026-08-17 11:35:33 -0500

    Eliminar comentarios de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  // Estado para controlar si mostramos la pantalla de inicio o la tienda
   const [showProducts, setShowProducts] = useState(false);
 
   const handleGetStarted = () => {
@@ -13,11 +12,9 @@ function App() {
   return (
     <div className="App">
       {!showProducts ? (
-        /* Pantalla de inicio (Landing Page) */
         <div className="landing-page">
           <h1>Paradise Nursery</h1>
           
-          {/* Aquí importamos el componente de la Tarea 2 */}
           <AboutUs />
           
           <button className="get-started-btn" onClick={handleGetStarted}>
@@ -25,7 +22,6 @@ function App() {
           </button>
         </div>
       ) : (
-        /* Pantalla de la tienda (Se construirá en la Tarea 6) */
         <div>
           <h2>Aquí irá la lista de plantas...</h2>
         </div>
