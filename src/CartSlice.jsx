668153333dbba486199872c167d5f2ff1b2095a0
Daniel Ovalle Tálaga <ovalle.talaga@gmail.com>
2026-08-17 11:40:28 -0500

    Implementada lógica de Redux con CartSlice y configuración del Store

---
+++ src/CartSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.name === newItem.name);
+      
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...newItem, quantity: 1 });
+      }
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload);
+    },
+    updateQuantity: (state, action) => {
+      const { name, amount } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      
+      if (itemToUpdate) {
+        itemToUpdate.quantity = amount;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
